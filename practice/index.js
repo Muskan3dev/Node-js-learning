f8286e5fa462f0de466a5e57ad0b8bc4e9b64570
Muskan3dev <muskannovember3@gmail.com>
2023-12-03 23:52:43 +0530

    Format

--- practice/index.js
+++ practice/index.js
@@ -7,24 +7,24 @@
    Now delete the file and the folder
    */
 
-const fs = require('fs');
+const fs = require("fs");
 //creating a directory
 
-//fs.mkdirSync("node");
+fs.mkdirSync("node");
 
 //creating a file inside the directory
 
-//fs.writeFileSync("json.txt","Node js is runtime enviroment for javascript");
+fs.writeFileSync("json.txt", "Node js is runtime enviroment for javascript");
 
 //Updating the  file content
 
-//fs.appendFileSync("practice/json.txt", "It is  built on chrome's V8 engine");
+fs.appendFileSync("practice/json.txt", "It is  built on chrome's V8 engine");
 
 //Read content of the file
 
-/* const data=fs.readFileSync("node/json.txt","utf8");
-console.log({"data":data}); */
+const data = fs.readFileSync("node/json.txt", "utf8");
+console.log({ data: data });
 
 //Rename file
 
-fs.renameSync("node/json.txt","node/json-Data.txt");
+fs.renameSync("node/json.txt", "node/json-Data.txt");
