4250461dc77c3504bb05a5f934205aaaabfb43ce
Muskan3dev <muskannovember3@gmail.com>
2023-12-03 20:05:39 +0530

    Added  fs.appendFilesync() method

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ const fs = require('fs');
 
 //creating a new file
 fs.writeFileSync("input.txt","JS is synchronous and Single threaded language");
+
+//Appending the content in file
+fs.appendFileSync("input.txt","JS is high level,interpreted scripting language");
