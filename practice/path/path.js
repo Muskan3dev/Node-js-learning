883d4cb254e5217d33fa99c6c209896d0f21b18f
Muskan3dev <muskannovember3@gmail.com>
2023-12-04 15:14:21 +0530

    Added path.extname() method

--- practice/path/path.js
+++ practice/path/path.js
@@ -1,5 +1,8 @@
 //Path module-node:path module provide utilities for working with file and directory paths.
 const path = require("path");
 
-console.log(path.dirname({dirname:"C:\Users\Muskan\Desktop\Node-js-learning\practice\practice-02"}));
+//dirname
+console.log(path.dirname("C:\Users\Muskan\Desktop\Node-js-learning\practice\practice-02"));
 
+//extension of the file
+console.log(path.extname("C:\Users\Muskan\Desktop\Node-js-learning\practice\practice-02/path.js"));
