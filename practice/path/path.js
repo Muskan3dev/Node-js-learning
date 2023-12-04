c087ae049bdc0c6d35165622c0ccf1fe898bbe73
Muskan3dev <muskannovember3@gmail.com>
2023-12-04 15:21:17 +0530

    Added path.basename() method

--- practice/path/path.js
+++ practice/path/path.js
@@ -6,3 +6,6 @@ console.log(path.dirname("C:\Users\Muskan\Desktop\Node-js-learning\practice\prac
 
 //extension of the file
 console.log(path.extname("C:\Users\Muskan\Desktop\Node-js-learning\practice\practice-02/path.js"));
+
+//filename
+console.log(path.basename("C:\Users\Muskan\Desktop\Node-js-learning\practice\practice-02/path.js"));
