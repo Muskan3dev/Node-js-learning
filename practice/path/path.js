1d3d0634464a1e9493cc6220e2aaa4a76f4dee61
Muskan3dev <muskannovember3@gmail.com>
2023-12-04 15:00:31 +0530

    Added path.dirname() method

---
+++ practice/path/path.js
@@ -0,0 +1,5 @@
+//Path module-node:path module provide utilities for working with file and directory paths.
+const path = require("path");
+
+console.log(path.dirname({dirname:"C:\Users\Muskan\Desktop\Node-js-learning\practice\practice-02"}));
+
