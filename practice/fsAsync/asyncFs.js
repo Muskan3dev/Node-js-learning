0024aaecf4244c3cc0158dcdc9f17c0108773b8b
Muskan3dev <muskannovember3@gmail.com>
2023-12-04 10:11:21 +0530

    Added fs.appendFile() asynchronous method

--- practice/fsAsync/asyncFs.js
+++ practice/fsAsync/asyncFs.js
@@ -1,7 +1,16 @@
-const fs = require('fs');
+const fs = require("fs");
 
 //Creating file Asynchronously
-fs.writeFile("read.txt","chrome's V8 engine is written in C++",(err) =>{
-console.log("File created successfully");
+fs.writeFile("read.txt", "chrome's V8 engine is written in C++", (err) => {
+  console.log("File created successfully");
+  console.log("error");
 });
 
+//Updating file content
+fs.appendFile(
+  "read.txt",
+  " Node.js was initially written by Ryan Dahl in 2009 ",
+  (err) => {
+    console.log("Updated Successfully");
+  }
+);
